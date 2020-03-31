08b2f45ebd2836994c8334001066d02ac65df85c
Marco Silvestri <m.silvestri03@gmail.com>
2020-03-31 17:44:18 +0200

    Styled, done.

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ Ask the user for a name, insert it in a populated array. Then print it AZ sorted
 
 */
 
+//Declare references
+var fieldBefore = document.getElementById('list-before');
+var fieldAfter = document.getElementById('list-after');
+var fieldBeforePos = document.getElementById('pos-before');
+var fieldAfterPos = document.getElementById('pos-after');
+
 //Declare variables
  var usersArray = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
  var usersArraySorted;
  var usersInput;
  var usersInputInArray;
-
+ var fieldAfterOld;
+ var fieldBeforeOld;
 
 //Force a non-empty input
  do {
@@ -19,6 +26,21 @@ Ask the user for a name, insert it in a populated array. Then print it AZ sorted
 //Add the input to the array and sort it, giving a before/after output
 usersArray.push(usersInput);
 usersInputInArray =  usersArray.indexOf(usersInput);
-console.log('Original position is ', usersInputInArray, 'in the array ', usersArray);
+for (var i = 0; i < usersArray.length; i++) {
+  fieldBeforeOld = fieldBefore.innerHTML;
+  fieldBefore.innerHTML = fieldBeforeOld + '<li>' + usersArray[i] + '</li>';
+}
+
+fieldBeforePos.innerHTML = 'Original position was ' + usersInputInArray;
+
+//console.log('Original position was ', usersInputInArray, 'in the array ', usersArray);
+
 usersArraySorted = usersArray.sort();
-console.log('Sorted position is ', usersArraySorted.indexOf(usersInput), 'in the array ', usersArraySorted);
+for (var i = 0; i < usersArraySorted.length; i++) {
+  fieldAfterOld = fieldAfter.innerHTML;
+  fieldAfter.innerHTML = fieldAfterOld + '<li>' + usersArraySorted[i] + '</li>';
+}
+
+fieldAfterPos.innerHTML = 'Sorted position is ' + usersArraySorted.indexOf(usersInput);
+
+//console.log('Sorted position is ', usersArraySorted.indexOf(usersInput), 'in the array ', usersArraySorted);
